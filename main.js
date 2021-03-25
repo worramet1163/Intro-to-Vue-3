7cb99e4b40dbb017936507b4fdcec3c6f95d4918
worramet1163 <6206021621163@fitm.kmutnb.ac.th>
2021-03-25 10:57:51 +0700

    ll

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            // solution
-            onSale: true
-            // solution
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inventory: 100,
+      detail: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+        { id: 2236, color: "red" },
+      ],
+      cart: 0,
+      // solution
+      onSale: true,
+      // solution
+    };
+  },
+});
