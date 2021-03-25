d63de7b0c47a72c540da4886a29bca9720655c27
worramet1163 <6206021621163@fitm.kmutnb.ac.th>
2021-03-25 10:26:43 +0700

    kk

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      des: "super sock",
+      image: "./assets/images/socks_green.jpg",
+      inventory: 8,
+      // solution
+      url: "https://www.vuemastery.com/",
+      // solution
+    };
+  },
+});
