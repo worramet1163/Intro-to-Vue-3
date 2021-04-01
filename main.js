49872ee2f432dcf1c4c182cbdef5fb647f15ff6f
worramet1163 <6206021621163@fitm.kmutnb.ac.th>
2021-04-01 10:08:13 +0700

    e

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      cart: 0,
+      brand: "Doggo",
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      seletedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateVariant(index) {
+      this.seletedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.seletedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.seletedVariant].quantity;
+    },
+  },
+});
