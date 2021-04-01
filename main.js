9960e9889652ed55948ef5c25a5d690b3e1bc66e
worramet1163 <6206021621163@fitm.kmutnb.ac.th>
2021-04-01 09:45:33 +0700

    lo

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    }
-})
+  data() {
+    return {
+      activeClass: true,
+      cart: 0,
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: false,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removefromcart() {
+      if (this.cart > 0) this.cart -= 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
+});
