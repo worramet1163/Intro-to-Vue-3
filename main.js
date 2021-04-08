be7f06ab8d99396b66c2a33a4da8888b050622b9
worramet1163 <6206021621163@fitm.kmutnb.ac.th>
2021-04-08 08:54:10 +0700

    f

--- main.js
+++ main.js
@@ -2,44 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      brand: "Doggo",
-      product: "Socks",
-
-      seletedVariant: 0,
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      variants: [
-        {
-          id: 2234,
-          color: "green",
-          image: "./assets/images/socks_green.jpg",
-          quantity: 50,
-        },
-        {
-          id: 2235,
-          color: "blue",
-          image: "./assets/images/socks_blue.jpg",
-          quantity: 0,
-        },
-      ],
     };
   },
-  methods: {
-    addToCart() {
-      this.cart += 1;
-    },
-    updateVariant(index) {
-      this.seletedVariant = index;
-    },
-  },
-  computed: {
-    title() {
-      return this.brand + " " + this.product;
-    },
-    image() {
-      return this.variants[this.seletedVariant].image;
-    },
-    inStock() {
-      return this.variants[this.seletedVariant].quantity;
-    },
-  },
+  methods: {},
 });
